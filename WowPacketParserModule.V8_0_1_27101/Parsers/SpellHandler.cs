1b6fd22f28b9b81eb7e05143b77d0a97b908e726
ModoX <moardox@gmail.com>
2018-08-06 18:47:16 +0200

    using signedness of client now

--- WowPacketParserModule.V8_0_1_27101/Parsers/SpellHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/SpellHandler.cs
@@ -42,6 +42,19 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
             ReadTalentInfoUpdate(packet, "Info");
         }
 
+        public static void ReadSpellHealPrediction(Packet packet, params object[] idx)
+        {
+            packet.ReadUInt32("Points", idx);
+            packet.ReadByte("Type", idx);
+            packet.ReadPackedGuid128("BeaconGUID", idx);
+        }
+
+        public static void ReadSpellPowerData(Packet packet, params object[] idx)
+        {
+            packet.ReadUInt32("Cost", idx);
+            packet.ReadByteE<PowerType>("Type", idx);
+        }
+
         public static void ReadSpellCastData(Packet packet, params object[] idx)
         {
             packet.ReadPackedGuid128("CasterGUID", idx);
@@ -53,9 +66,9 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
             var spellID = packet.ReadUInt32<SpellId>("SpellID", idx);
             packet.ReadUInt32("SpellXSpellVisualID", idx);
 
-            packet.ReadUInt32("CastFlags", idx); // all three are signed in client but we use them as unsigned for a while, wotevs
-            packet.ReadUInt32("CastFlagsEx", idx);
-            packet.ReadUInt32("CastTime", idx);
+            packet.ReadInt32("CastFlags", idx); 
+            packet.ReadInt32("CastFlagsEx", idx);
+            packet.ReadInt32("CastTime", idx);
 
             V6_0_2_19033.Parsers.SpellHandler.ReadMissileTrajectoryResult(packet, idx, "MissileTrajectory");
 
@@ -65,7 +78,7 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
 
             V6_0_2_19033.Parsers.SpellHandler.ReadCreatureImmunities(packet, idx, "Immunities");
 
-            V6_0_2_19033.Parsers.SpellHandler.ReadSpellHealPrediction(packet, idx, "Predict");
+            ReadSpellHealPrediction(packet, idx, "Predict");
 
             packet.ResetBitReader();
 
@@ -90,7 +103,7 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
                 packet.ReadPackedGuid128("MissTarget", idx, i);
 
             for (var i = 0; i < remainingPowerCount; ++i)
-                V6_0_2_19033.Parsers.SpellHandler.ReadSpellPowerData(packet, idx, "RemainingPower", i);
+                ReadSpellPowerData(packet, idx, "RemainingPower", i);
 
             if (hasRuneData)
                 V7_0_3_22248.Parsers.SpellHandler.ReadRuneData(packet, idx, "RemainingRunes");
@@ -102,9 +115,9 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
         public static void ReadSpellCastLogData(Packet packet, params object[] idx)
         {
             packet.ReadInt64("Health", idx);
-            packet.ReadInt32("AttackPower", idx);
-            packet.ReadInt32("SpellPower", idx);
-            packet.ReadInt32("UnkInt32_801", idx);
+            packet.ReadUInt32("AttackPower", idx);
+            packet.ReadUInt32("SpellPower", idx);
+            packet.ReadUInt32("UnkUInt32_801", idx);
 
             packet.ResetBitReader();
 
@@ -113,9 +126,9 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
             // SpellLogPowerData
             for (var i = 0; i < spellLogPowerDataCount; ++i)
             {
-                packet.ReadInt32("PowerType", idx, i);
-                packet.ReadInt32("Amount", idx, i);
-                packet.ReadInt32("Cost", idx, i);
+                packet.ReadUInt32("PowerType", idx, i);
+                packet.ReadUInt32("Amount", idx, i);
+                packet.ReadUInt32("Cost", idx, i);
             }
         }
 
