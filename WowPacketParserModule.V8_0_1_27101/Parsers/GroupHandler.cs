7836429e3853e099b910b803df646a83c2a93270
Unknown <torti-esser@web.de>
2018-08-20 03:51:37 +0200

    fixed SMSG_PARTY_UPDATE

---
+++ WowPacketParserModule.V8_0_1_27101/Parsers/GroupHandler.cs
@@ -0,0 +1,82 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+using WowPacketParser.Store;
+using WowPacketParser.Store.Objects;
+
+namespace WowPacketParserModule.V8_0_1_27101.Parsers
+{
+    public static class GroupHandler
+    {
+        [Parser(Opcode.SMSG_PARTY_UPDATE)]
+        public static void HandlePartyUpdate(Packet packet)
+        {
+            packet.ReadUInt16("PartyFlags");
+            packet.ReadByte("PartyIndex");
+            packet.ReadByte("PartyType");
+
+            packet.ReadInt32("MyIndex");
+            packet.ReadPackedGuid128("PartyGUID");
+            packet.ReadInt32("SequenceNum");
+            packet.ReadPackedGuid128("LeaderGUID");
+
+            var playerCount = packet.ReadInt32("PlayerListCount");
+            var hasLFG = packet.ReadBit("HasLfgInfo");
+            var hasLootSettings = packet.ReadBit("HasLootSettings");
+            var hasDifficultySettings = packet.ReadBit("HasDifficultySettings");
+
+            for (int i = 0; i < playerCount; i++)
+            {
+                packet.ResetBitReader();
+                var playerNameLength = packet.ReadBits(6);
+                var bnetIdBits = packet.ReadBits(4); // either 4 or 0
+                packet.ReadBit("FromSocialQueue", i);
+                packet.ReadBit("Unk801_Bit", i);
+
+                packet.ReadPackedGuid128("Guid", i);
+
+                packet.ReadByte("Status", i);
+                packet.ReadByte("Subgroup", i);
+                packet.ReadByte("Flags", i);
+                packet.ReadByte("RolesAssigned", i);
+                packet.ReadByteE<Class>("Class", i);
+
+                packet.ReadWoWString("Name", playerNameLength, i);
+                if (bnetIdBits > 0) // according to implementation of CDataStore::GetCString in 8.0.1
+                    packet.ReadCString("BNetIdString", i);
+            }
+
+            packet.ResetBitReader();
+
+            if (hasLootSettings)
+            {
+                packet.ReadByte("Method", "PartyLootSettings");
+                packet.ReadPackedGuid128("LootMaster", "PartyLootSettings");
+                packet.ReadByte("Threshold", "PartyLootSettings");
+            }
+
+            if (hasDifficultySettings)
+            {
+                packet.ReadInt32("DungeonDifficultyID");
+                packet.ReadInt32("RaidDifficultyID");
+                packet.ReadInt32("LegacyRaidDifficultyID");
+            }
+
+            if (hasLFG)
+            {
+                packet.ResetBitReader();
+
+                packet.ReadByte("MyFlags");
+                packet.ReadInt32("Slot");
+                packet.ReadInt32("MyRandomSlot");
+                packet.ReadByte("MyPartialClear");
+                packet.ReadSingle("MyGearDiff");
+                packet.ReadByte("MyStrangerCount");
+                packet.ReadByte("MyKickVoteCount");
+                packet.ReadByte("BootCount");
+                packet.ReadBit("Aborted");
+                packet.ReadBit("MyFirstReward");
+            }
+        }
+    }
+}
