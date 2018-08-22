5ca83ca434801cd9497ad30f1fb151973574df5c
ModoX <moardox@gmail.com>
2018-08-22 15:39:30 +0200

    implemented CMSG_TOGGLE_WARMODE

---
+++ WowPacketParserModule.V8_0_1_27101/Parsers/CombatHandler.cs
@@ -0,0 +1,15 @@
+﻿using WowPacketParser.Enums;
+using WowPacketParser.Misc;
+using WowPacketParser.Parsing;
+
+namespace WowPacketParserModule.V8_0_1_27101.Parsers
+{
+    public static class CombatHandler
+    {
+        [Parser(Opcode.CMSG_TOGGLE_WARMODE)]
+        public static void HandleToggleWarmode(Packet packet)
+        {
+            packet.ReadBit("Enable");
+        }
+    }
+}
