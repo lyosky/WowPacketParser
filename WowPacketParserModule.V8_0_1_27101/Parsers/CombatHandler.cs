5e59babf2210774affaaf1be354b715ff4318563
Unknown <torti-esser@web.de>
2018-08-25 22:02:57 +0200

    Fix CMSG_SET_WAR_MODE

--- WowPacketParserModule.V8_0_1_27101/Parsers/CombatHandler.cs
+++ WowPacketParserModule.V8_0_1_27101/Parsers/CombatHandler.cs
@@ -6,7 +6,7 @@ namespace WowPacketParserModule.V8_0_1_27101.Parsers
 {
     public static class CombatHandler
     {
-        [Parser(Opcode.CMSG_TOGGLE_WARMODE)]
+        [Parser(Opcode.CMSG_SET_WAR_MODE)]
         public static void HandleToggleWarmode(Packet packet)
         {
             packet.ReadBit("Enable");
